581db905545ac2320975e193155dccd03f8d65ea
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-27 02:18:07 -0800

    feat:  Adding redux-devtools-extension dependancy.

--- bank-demo/src/store/store.js
+++ bank-demo/src/store/store.js
@@ -2,12 +2,12 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import { thunk } from "redux-thunk";
 import accountReducer from "../features/accounts/accountSlicer";
 import customerReducer from "../features/customers/customerSlicer";
-
+import {composeWithDevTools} from 'redux-devtools-extension';
 const reducerMap = {
   account: accountReducer,
   customer: customerReducer,
 };
 const rootReducer = combineReducers(reducerMap);
 
-const store = createStore(rootReducer,applyMiddleware(thunk));
+const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk)));
 export default store;
